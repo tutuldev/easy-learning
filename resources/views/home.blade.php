8deee4239f52c5bc36c84babce53b4580dde1272
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-04-01 16:01:37 +0600

    javascript,php,python,laravel,sql design section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -228,9 +228,9 @@
 
     </section>
     {{-- banner section end --}}
-    {{-- html learn seaction  --}}
-    <section class="html-learn-section mt-[-90px]">
-        <div class="pb-16 pt-32 bg-[#D9EEE1] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20">
+    {{-- html learn section  --}}
+    <section class="html-learn-section mt-[-90px] ">
+        <div class="pb-16 pt-32 bg-[#D9EEE1] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20 ">
             <div class="html-nav text-center">
                 <h2 class="text-7xl font-bold">HTML</h2>
                 <p class="font-semibold mb-5 mt-8">The language for building web pages</p>
@@ -248,7 +248,7 @@
             <div class="html-editor sm:flex items-center justify-center hidden">
                 <div class="w-96  bg-gray-200 rounded-lg shadow-2xl  px-5">
                     <h3 class="text-xl font-bold py-5">HTML Example:</h3>
-                    <div class="border-l-4 border-green-400 p-2 bg-white">
+                    <div class="border-l-4 min-h-[250px] border-green-400 p-2 bg-white">
 <pre>
 &lt;!DOCTYPE html&gt;
 &lt;html&gt;
@@ -276,7 +276,7 @@
     </section>
 
     {{-- learn css section --}}
-    <section class="html-learn-section ">
+    <section class="css-learn-section ">
         <div class="py-10 bg-[#FFF4A3] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20">
             <div class="css-nav text-center">
                 <h2 class="text-7xl font-bold">CSS</h2>
@@ -294,7 +294,7 @@
             <div class="css-editor sm:flex items-center justify-center hidden">
                 <div class="w-96  bg-gray-200 rounded-lg shadow-2xl  px-5">
                     <h3 class="text-xl font-bold py-5">CSS Example:</h3>
-                    <div class="border-l-4 border-green-400 p-2 bg-white">
+                    <div class="border-l-4 min-h-[250px] border-green-400 p-2 bg-white">
 <pre>
 &lt;style&gt;
 body {
@@ -324,7 +324,219 @@ p {
         </div>
     </section>
 
+    {{-- learn javascript section  --}}
+    <section class="javascript-learn-section ">
+        <div class="py-10 bg-[#261F1E] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20">
+            <div class="css-nav text-center">
+                <h2 class="text-6xl text-white font-bold">JavaScript</h2>
+                <p class="font-semibold text-white mb-5 mt-8">The language for programming web pages</p>
 
+                <!-- Button Section -->
+                <div class="flex flex-col items-center space-y-4 mt-4">
+                    <a href="#" class="rounded-full text-white bg-green-500 px-7 py-3 min-w-[280px] max-w-[350px] text-center">Learn JavaScript</a>
+                    <a href="#" class="rounded-full bg-orange-300 px-7 py-3 min-w-[280px] max-w-[350px] text-center">JavaScript Reference</a>
+                    <a href="#" class="rounded-full text-white bg-black px-7 py-3 min-w-[280px] max-w-[350px] text-center">Get Certified</a>
+                </div>
+            </div>
+
+            <!-- JavaScript Editor Section -->
+            <div class="JavaScript-editor sm:flex items-center justify-center hidden">
+                <div class="w-96  bg-gray-200 rounded-lg shadow-2xl  px-5">
+                    <h3 class="text-xl font-bold py-5">JavaScript Example:</h3>
+                    <div class="border-l-4 min-h-[250px] border-green-400 p-2 bg-white">
+<pre>
+&lt;button onclick=
+&quot;myFunction()&quot;&gt;Click Me!&lt;/button&gt;
+
+&lt;script&gt;
+function myFunction() {
+  let x = document.getElementById
+  (&quot;demo&quot;);
+  x.style.fontSize = &quot;25px&quot;;
+  x.style.color = &quot;red&quot;;
+}
+&lt;/script&gt;
+</pre>
+
+
+
+                    </div>
+                    <a href="#" class="bg-green-500 px-10 py-3 rounded-full inline-block text-sm font-bold my-5 text-white">Try it Yourself</a>
+
+                </div>
+                <div class="code">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- learn PHP  --}}
+    <section class="PHP-learn-section ">
+        <div class="py-10 bg-[#FFC0C7] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20">
+            <div class="css-nav text-center">
+                <h2 class="text-6xl  font-bold">PHP</h2>
+                <p class="font-semibold  mb-5 mt-8">A web server programming language</p>
+
+                <!-- Button Section -->
+                <div class="flex flex-col items-center space-y-4 mt-4">
+                    <a href="#" class="rounded-full text-white bg-green-500 px-7 py-3 min-w-[280px] max-w-[350px] text-center">Learn PHP</a>
+                    <a href="#" class="rounded-full bg-orange-300 px-7 py-3 min-w-[280px] max-w-[350px] text-center">PHP Reference</a>
+                    <a href="#" class="rounded-full text-white bg-black px-7 py-3 min-w-[280px] max-w-[350px] text-center">Get Certified</a>
+                </div>
+            </div>
+
+            <!-- PHP Editor Section -->
+            <div class="PHP-editor sm:flex items-center justify-center hidden">
+                <div class="w-96  bg-gray-200 rounded-lg shadow-2xl  px-5">
+                    <h3 class="text-xl font-bold py-5">PHP Example:</h3>
+                    <div class="border-l-4 min-h-[250px] border-green-400 p-2 bg-white">
+<pre>
+&lt;?php
+  echo &quot;Hello, World!&quot;;
+
+  $name = &quot;Moumita&quot;;
+  echo &quot;&lt;br&gt;Welcome, &quot; . $name . &quot;!&quot;;
+?&gt;
+</pre>
+
+
+
+
+                    </div>
+                    <a href="#" class="bg-green-500 px-10 py-3 rounded-full inline-block text-sm font-bold my-5 text-white">Try it Yourself</a>
+
+                </div>
+                <div class="code">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- learn Python  --}}
+    <section class="Python-learn-section ">
+        <div class="py-10 bg-[#F3ECEA] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20">
+            <div class="css-nav text-center">
+                <h2 class="text-6xl  font-bold">Python</h2>
+                <p class="font-semibold  mb-5 mt-8">A popular programming language</p>
+
+                <!-- Button Section -->
+                <div class="flex flex-col items-center space-y-4 mt-4">
+                    <a href="#" class="rounded-full text-white bg-green-500 px-7 py-3 min-w-[280px] max-w-[350px] text-center">Learn Python</a>
+                    <a href="#" class="rounded-full bg-orange-300 px-7 py-3 min-w-[280px] max-w-[350px] text-center">Python Reference</a>
+                    <a href="#" class="rounded-full text-white bg-black px-7 py-3 min-w-[280px] max-w-[350px] text-center">Get Certified</a>
+                </div>
+            </div>
+
+            <!-- Python Editor Section -->
+            <div class="Python-editor  sm:flex items-center justify-center hidden">
+                <div class="w-96   bg-gray-200 rounded-lg shadow-2xl  px-5">
+                    <h3 class="text-xl font-bold py-5">Python Example:</h3>
+                    <div class="border-l-4 min-h-[250px] border-green-400 p-2 bg-white">
+<pre>
+ if 5 &gt; 2:
+    print(&quot;Five is greater than two!&quot;)
+</pre>
+
+
+
+
+                    </div>
+                    <a href="#" class="bg-green-500 px-10 py-3 rounded-full inline-block text-sm font-bold my-5 text-white">Try it Yourself</a>
+
+                </div>
+                <div class="code">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- learn Laravel  --}}
+    <section class="Laravel-learn-section ">
+        <div class="py-10 bg-[#261F1E] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20">
+            <div class="css-nav text-center">
+                <h2 class="text-6xl text-white font-bold">Laravel</h2>
+                <p class="font-semibold text-white mb-5 mt-8">A PHP framework for building modern web applications.</p>
+
+                <!-- Button Section -->
+                <div class="flex flex-col items-center space-y-4 mt-4">
+                    <a href="#" class="rounded-full text-white bg-green-500 px-7 py-3 min-w-[280px] max-w-[350px] text-center">Learn Laravel</a>
+                    <a href="#" class="rounded-full bg-orange-300 px-7 py-3 min-w-[280px] max-w-[350px] text-center">Laravel Reference</a>
+                    <a href="#" class="rounded-full text-white bg-black px-7 py-3 min-w-[280px] max-w-[350px] text-center">Get Certified</a>
+                </div>
+            </div>
+
+            <!-- Laravel Editor Section -->
+            <div class="Laravel-editor sm:flex items-center justify-center hidden">
+                <div class="w-96  bg-gray-200 rounded-lg shadow-2xl  px-5">
+                    <h3 class="text-xl font-bold py-5">Laravel Example:</h3>
+                    <div class="border-l-4 min-h-[250px] border-green-400 p-2 bg-white">
+<pre>
+&lt;?php
+use App\Models\User;
+
+$user = User::where
+('email', 'example@example.com')
+-&gt;first();
+
+if ($user) {
+echo 'User found: ' . $user-&gt;name;
+} else {
+echo 'User not found';
+}
+?&gt;
+</pre>
+
+
+
+                    </div>
+                    <a href="#" class="bg-green-500 px-10 py-3 rounded-full inline-block text-sm font-bold my-5 text-white">Try it Yourself</a>
+
+                </div>
+                <div class="code">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- learn SQL  --}}
+    <section class="SQL-learn-section ">
+        <div class="py-10 bg-[#96D4D4] flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20">
+            <div class="css-nav text-center">
+                <h2 class="text-6xl text-white font-bold">SQL</h2>
+                <p class="font-semibold text-white mb-5 mt-8">A language for accessing databases
+
+                </p>
+
+                <!-- Button Section -->
+                <div class="flex flex-col items-center space-y-4 mt-4">
+                    <a href="#" class="rounded-full text-white bg-green-500 px-7 py-3 min-w-[280px] max-w-[350px] text-center">Learn SQL</a>
+                    <a href="#" class="rounded-full bg-orange-300 px-7 py-3 min-w-[280px] max-w-[350px] text-center">SQL Reference</a>
+                    <a href="#" class="rounded-full text-white bg-black px-7 py-3 min-w-[280px] max-w-[350px] text-center">Get Certified</a>
+                </div>
+            </div>
+
+            <!-- SQL Editor Section -->
+            <div class="SQL-editor sm:flex items-center justify-center hidden">
+                <div class="w-96  bg-gray-200 rounded-lg shadow-2xl  px-5">
+                    <h3 class="text-xl font-bold py-5">SQL Example:</h3>
+                    <div class="border-l-4 min-h-[250px] border-green-400 p-2 bg-white">
+<pre>
+SELECT * FROM Customers
+WHERE Country='Mexico';
+</pre>
+
+
+
+                    </div>
+                    <a href="#" class="bg-green-500 px-10 py-3 rounded-full inline-block text-sm font-bold my-5 text-white">Try it Yourself</a>
+
+                </div>
+                <div class="code">
+
+                </div>
+            </div>
+        </div>
+    </section>
 
     <!-- Swiper JS -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
